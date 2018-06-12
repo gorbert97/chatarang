faf657c15d92f484c38ebe5e9203e2b8d7143f79
Tim Kurowski <timk10@comcast.net>
2018-06-12 14:27:40 -0400

    updated the styles of the components

--- src/MessageList.js
+++ src/MessageList.js
@@ -4,7 +4,7 @@ import Message from './Message'
 
 const MessageList = (props) => {
   return (
-    <div className="MessageList">
+    <div className="MessageList" style={styles.messageList} >
       {
         props.messages.map(msg => <Message key={msg.id} message={msg} />)
       }
@@ -12,4 +12,20 @@ const MessageList = (props) => {
   )
 }
 
+const styles = {
+  messageList: {
+    backgroundColor: 'white',
+  flex: 1,
+  paddingBottom: '1rem',
+  overflowY: 'scroll',
+  },
+
+  roomAnouncement: {
+    padding: '2rem 1rem',
+  },
+
+  h3:{
+    fontSize: '1.5rem',
+  },
+}
 export default MessageList
